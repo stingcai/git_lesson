8f91ec263739988f5f9ac06c2cfdee1abe86a509
Sting Cai <mij@hpe.com>
2017-06-20 18:55:37 +0800

    changed 3 to 4

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,6 @@
 def my_square(x):
-	=== take a value and returns the squared value.
-	===
+	""" take a value and returns the squared value.
+	"""
 	return(x **2)
 
-print(my_square(3))
+print(my_square(4))
