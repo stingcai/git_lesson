a09296d06b56b2eaf5577c0b5706973e845febe2
Sting Cai <mij@hpe.com>
2017-06-20 19:06:39 +0800

    changed 3 to 4

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,6 @@
 def my_square(x):
 	""" take a value and returns the squared value.
-<<<<<<< HEAD
-=======
 	uses the ** operator
->>>>>>> b8660833e987a81aa0cf42e05ff09f2d031c1563
 	"""
 	return(x **2)
 
