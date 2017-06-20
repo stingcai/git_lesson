b8660833e987a81aa0cf42e05ff09f2d031c1563
stingcai <stingcai@outlook.com>
2017-06-20 18:58:09 +0800

    added documentation about the ** operator

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,7 @@
 def my_square(x):
-	=== take a value and returns the squared value.
-	===
+	""" take a value and returns the squared value.
+	uses the ** operator
+	"""
 	return(x **2)
 
 print(my_square(3))
